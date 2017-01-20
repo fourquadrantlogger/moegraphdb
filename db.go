e76cbf24c9b3b495bd40ca7de2d63bff48c7e815
timeloveboy <734991033@qq.com>
2017-01-20 14:35:00 +0800

    ~

--- db.go
+++ db.go
@@ -1,20 +1,2 @@
 package main
 
-import "sync"
-
-type (
-	V struct{
-		sync.RWMutex
-		vid uint64
-		fans map[uint64]*V
-		like map[uint64]*V
-	}
-)
-
-var (
-	Vlist []*V
-)
-
-func init()  {
-	Vlist=make([]*V,3000000000)
-}
--- webapp.go
+++ webapp.go
@@ -1,7 +1,88 @@
 package main
 
-func main() {
-	Vlist[0]=&V{
-		vid:102445635345,
+import (
+	"net/http"
+	"net/url"
+	"fmt"
+	"strconv"
+
+	"sync"
+)
+
+type (
+	User struct{
+		Lock_fans sync.RWMutex
+		Lock_likes sync.RWMutex
+		Vid uint
+		Fans map[uint]*User
+		Likes map[uint]*User
 	}
+)
+
+var (
+	UserArray []*User
+)
+
+func init()  {
+	UserArray =make([]*User,50000000)
+	for i,_:=range UserArray{
+		UserArray[i]=new(User)
+		UserArray[i].Vid=uint(i)
+		UserArray[i].Fans=make( map[uint]*User )
+		UserArray[i].Likes=make( map[uint]*User )
+	}
+}
+func (this *User)Getlikes()[]uint{
+	result:=make([]uint,len(this.Likes))
+	i:=0
+	for k,_:=range this.Likes{
+		result[i]=k
+		i++
+	}
+	return result
+}
+func (this *User)Getfans()[]uint{
+	result:=make([]uint,len(this.Fans))
+	i:=0
+	for k,_:=range this.Fans{
+		result[i]=k
+		i++
+	}
+	return result
+}
+func main() {
+
+	http.HandleFunc("/like", func(w http.ResponseWriter,r *http.Request){
+		m,_:=url.ParseQuery(r.URL.RawQuery)
+		vid,_:=strconv.Atoi(m["vid"][0])
+		switch r.Method {
+		case http.MethodGet:{
+			w.Write([]byte(fmt.Sprint(UserArray[vid].Getlikes())))
+			}
+		case http.MethodPost:
+			star,_:=strconv.Atoi(m["star"][0])
+			UserArray[vid].Likes[uint(star)]=UserArray[uint(star)]
+			UserArray[uint(star)].Fans[uint(vid)]=UserArray[uint(vid)]
+		}
+
+	})
+
+	http.HandleFunc("/fans", func(w http.ResponseWriter,r *http.Request){
+		m,_:=url.ParseQuery(r.URL.RawQuery)
+		vid,_:=strconv.Atoi(m["vid"][0])
+		switch r.Method {
+		case http.MethodGet:{
+			w.Write([]byte(fmt.Sprint(UserArray[vid].Getfans())))
+		}
+		case http.MethodPost:
+			fan,_:=strconv.Atoi(m["fan"][0])
+			UserArray[vid].Fans[uint(fan)]=UserArray[uint(fan)]
+			UserArray[uint(fan)].Likes[uint(vid)]=UserArray[uint(vid)]
+		}
+
+	})
+
+	fmt.Println("start http server")
+	err :=http.ListenAndServe(":8010",nil)
+	panic(err)
 }
