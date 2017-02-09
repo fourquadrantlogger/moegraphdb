96e629165bf524b969e2a740e2fed4ba1c1403db
timeloveboy <734991033@qq.com>
2017-02-09 15:20:41 +0800

    ~

--- graphdb-loader/app.go
+++ graphdb-loader/app.go
@@ -85,6 +85,7 @@ func ReadLine(filePth string, hookfn func([]byte)) error {
 		hookfn(line)    //放在错误处理前面，即使发生错误，也会处理已经读取到的数据。
 		if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
 			if err == io.EOF {
+				fmt.Println("read ok", filePth)
 				return nil
 			}
 			return err
